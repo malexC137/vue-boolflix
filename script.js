da210bbe320ce70362fa06586ef7d5ea59c33c28
malexC137 <71665644+malexC137@users.noreply.github.com>
2021-05-17 13:50:43 +0200

    Milestone 4 done

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ new Vue({
         querySearch: "",
         myTmdbKey: "bc6f81aaa58a9888c0efcd14fa367c64",
         moviesList: [],
-        seriesList: []
+        seriesList: [],
+        imgUrlBasic: "https://image.tmdb.org/t/p/",
+        sizeUrl: "w154",
     },
     methods: {
         onSearchText() {
@@ -20,11 +22,63 @@ new Vue({
                 .then((resp) => {
                     this.moviesList = resp.data.results
                 }),
-            axios.get("https://api.themoviedb.org/3/search/tv?", axiosParams)
-                .then((result) => {
-                    this.seriesList = result.data.results
-                })
-
+                axios.get("https://api.themoviedb.org/3/search/tv?", axiosParams)
+                    .then((result) => {
+                        this.seriesList = result.data.results
+                    })
         },
+
     },
+    computed: {
+        // fullStars() {
+        //     const fullStarsArr = [];
+        //     const fullStars = []
+        //     this.moviesList.find((element) => {
+        //         let voteNum = Math.ceil(element.vote_average / 2)
+        //         for (var i = 0; i < voteNum; i++) {
+        //             fullStarsArr.push(i)
+        //             if (fullStarsArr.length === voteNum) {
+        //                 return fullStars.push(fullStarsArr)
+        //             }
+
+        //         }
+
+        //     })
+        //     return fullStars
+
+
+
+
+
+
+
+            // this.moviesList.filter((element) => {
+            //     const voteNum = Math.ceil(element.vote_average / 2)
+            //     for (let i = 0; i < this.moviesList.length; i++) {
+            //         fullStarsArr.push(voteNum)
+            //         for (let j = 0; j < voteNum; j++) {
+            //             fullStars.push(j)
+            //         }
+
+            //     }
+
+            // })
+            // return fullStars
+
+
+
+
+            // for (let i = 0; i < voteNum; i++) {
+            //     fullStarsArr.push(voteNum)
+            // }
+            // return fullStarsArr
+            // const fullStarsArr = [];
+
+
+
+
+
+        // }
+    }
+
 })

21dc9d16afb69862a02e11ab61c7a7896d02a288
malexC137 <71665644+malexC137@users.noreply.github.com>
2021-05-18 22:28:59 +0200

    Project improved

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ new Vue({
         seriesList: [],
         imgUrlBasic: "https://image.tmdb.org/t/p/",
         sizeUrl: "w185",
+        // currentMovie: null,
+        // selectedGenre: 'All'
     },
     methods: {
         onSearchText() {
@@ -28,5 +30,76 @@ new Vue({
                     })
         },
 
+        getImg(movie) {
+
+            if (movie.poster_path === null) {
+                return "img/posterPlaceholder.PNG"
+            } else {
+                return this.imgUrlBasic + this.sizeUrl + movie.poster_path
+            }
+        },
+
+        getFlag(movie) {
+            const flagsMap = {
+                en: "gb",
+                ko: "kr",
+                ja: "jp",
+                xx: "cn",
+                hi: "in",
+            };
+
+            if (flagsMap[movie.original_language]) {
+                return flagsMap[movie.original_language];
+            } else {
+                return movie.original_language;
+            }
+        },
+
+        getStars(movie) {
+            const movieVote = Math.round(movie.vote_average / 2);
+            const starsArr = [];
+
+            for (i = 1; i <= 5; i++) {
+                starsArr.push(i <= movieVote)
+            }
+            
+            return starsArr
+        },
+
+        getCast(movie) {
+            if (movie.actors) {
+                return;
+            }
+
+            const axiosOptions = {
+                params: {
+                    api_key: this.myTmdbKey,
+                    language: "it-IT"
+                }
+
+            }
+
+            const movieType = movie.serie ? "tv" : "movie";
+
+            axios.get(`https://api.themoviedb.org/3/${movieType}/${movie.id}/credits?`, axiosOptions)
+                .then(resp => {
+                    this.$set(movie, "actors", resp.data.cast);
+                });
+        }
+
     },
+    // computed: {
+    //     movieGenres(movie) {
+    //         const genresArr = []
+    //         const axiosOptions = {
+    //             api_key: this.myTmdbKey,
+    //             language: "it-IT"
+    //         }
+    //         axios.get(`https://api.themoviedb.org/3/genre/movie/list`, axiosOptions)
+    //             .then(resp => {
+    //                 genresArr.push(resp.data.genres)
+    //             })
+    //         return genresArr
+    //     }
+    // }
 })
